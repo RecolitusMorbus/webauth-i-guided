76291e2ec9f6161f3d2afe6678fe8a8a925ee1cc
Nolan Allen <nolan.r.allen@gmail.com>
2019-11-11 12:09:42 -0600

    2nd commit: in-lesson assignment complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
+  let { username, password } = req.body;
 
-  Users.add(user)
+  Users.add({ username, password: bcrypt.hashSync(password, 14) })
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -20,7 +21,7 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}.` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
